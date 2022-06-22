68ebf4baf41b83511cdac1c54a38b6878ba4e92d
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-06-22 01:08:59 -0400

    Search petfinder for pet information not working

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import time
 from selenium import webdriver
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://google.com")
+driver.get("https://www.petfinder.com/search/cats-for-adoption/ca/ontario/toronto/?age%5B0%5D=Young&distance=50")
+cats = driver.find_elements_by_class_name('petCard-body-details')
+
+for cat in cats:
+    print(cat.find_element_by_xpath('.//span[@class="u-isVisuallyHidden"]').text)
+
+time.sleep(10)
