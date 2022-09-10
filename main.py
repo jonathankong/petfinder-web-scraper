8f1a5127a971881e24c9e6495b5b6b9b9d5cee59
Jonathan Ong <30916534+jonathankong@users.noreply.github.com>
2022-09-10 13:39:15 -0400

    Update main.py
    Added cat links to keep track of; modified WebDriver code to use 'with'; testing sending email

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import win32com.client as win32
+import os 
+import base64
 
 catNames = []
+catLinkList = []
 
 DRIVER_PATH = "C:\Program Files (x86)\chromedriver.exe"
 
@@ -19,35 +23,53 @@ path = Path.cwd() / IMAGES_FOLDER
 path.mkdir(parents=True, exist_ok = True)
 
 s = Service(DRIVER_PATH)
-driver = webdriver.Chrome(service=s)
-driver.get("https://www.petfinder.com/search/cats-for-adoption/ca/ontario/toronto/?age%5B0%5D=Young&distance=50")
+with webdriver.Chrome(service=s) as driver: 
+    driver.get("https://www.petfinder.com/search/cats-for-adoption/ca/ontario/toronto/?age%5B0%5D=Young&distance=50")
 
-# Better than time.sleep because this will continue once element is loaded
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//pfdc-animal-search-results//div[@class='petCard-body-details-hdg']/span[@class='u-isVisuallyHidden']")))
+    # Better than time.sleep because this will continue once element is loaded
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//pfdc-animal-search-results//div[@class='petCard-body-details-hdg']/span[@class='u-isVisuallyHidden']")))
 
-cats = driver.find_elements(By.XPATH, "//pfdc-animal-search-results//div[@class='petCard-body-details-hdg']/span[@class='u-isVisuallyHidden']")
+    # cats = driver.find_elements(By.XPATH, "//pfdc-animal-search-results//div[@class='petCard-body-details-hdg']/span[@class='u-isVisuallyHidden']")
+    cats = driver.find_elements(By.XPATH, "//pfdc-animal-search-results//div[@class='petCard-body-details-hdg']/span[@data-test='Pet_Card_Pet_Details_List']")
+    catLinks = driver.find_elements(By.XPATH, "//pfdc-animal-search-results//a[@class='petCard-link']")
+    catImgs = driver.find_elements(By.XPATH, "//pfdc-animal-search-results//pfdc-lazy-load")
 
-imgs = driver.find_elements(By.XPATH, "//pfdc-animal-search-results//pfdc-lazy-load")
+    # String src = imgElement.getAttribute('src');
+    # BufferedImage bufferedImage = ImageIO.read(new URL(src));
+    # File outputfile = new File("saved.png");
+    # ImageIO.write(bufferedImage, "png", outputfile);
+    for i in range(0, len(catImgs)):
+        #Get names of cats
+        catName = cats[i].get_attribute('innerHTML')
+        #Remove any special characters from name
+        catName = re.sub(r'[^a-zA-Z0-9 ]', '', catName)
+        #Get link for cat
+        catLink = catLinks[i].get_attribute('href')
+        #Get the image source
+        src = catImgs[i].get_attribute('src')
+        #Save image with cat name
+        imagePath = f'{path}\{catName}.png'
+        #Save image
+        urllib.request.urlretrieve(src, imagePath)
+        
+        #Keep track of cats and their related links, not using collection because there could be multiple cats with the same name
+        catNames.append(catName)
+        catLinkList.append(catLink)
+        # print(cat.find_element(By.XPATH, "").text)
 
-# String src = imgElement.getAttribute('src');
-# BufferedImage bufferedImage = ImageIO.read(new URL(src));
-# File outputfile = new File("saved.png");
-# ImageIO.write(bufferedImage, "png", outputfile);
-for i in range(0, len(imgs)):
-    #Get names of cats
-    catName = cats[i].get_attribute('innerHTML').split(',', 1)[0]
-    #Remove any special characters from name
-    catName = re.sub(r'[^a-zA-Z0-9 ]', '', catName)
-    # get the image source``
-    src = imgs[i].get_attribute('src')
-    #Save image with cat name
-    imagePath = f'{path}\{catName}.png'
+encoded_image = base64.b64encode(open(f'{path}\{catNames[0]}.png', "rb").read()).decode("utf-8")
 
-    urllib.request.urlretrieve(src, imagePath)
-    
-    catNames.append(catName)
-    # print(cat.find_element(By.XPATH, "").text)
+outlook = win32.Dispatch('outlook.application')
 
-driver.close()
-time.sleep(10)
+mail = outlook.CreateItem(0)
+mail.Subject = 'Testing Email'
+mail.BodyFormat= 2
+mail.Body = 'Hello World'
+mail.To = 'test@gmail.com'
+mail.HTMLBody = f"""
+    <h1><a href="{catLinkList[0]}">{catNames[0]}</a></h1>
+    <img src="data:image/png;base64,{encoded_image}"/>
+"""
+mail.Display()
+#mail.Send()
 
